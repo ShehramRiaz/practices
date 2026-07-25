c656f7a73669b0e6efafdeeb26b39131873907cb
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 12:26:18 +0500

    Learned Comparison Operators

---
+++ JS/03 Operators/03 Comparison Operators/script.js
@@ -0,0 +1,22 @@
+console.log(5 == 5); // true
+console.log(5 == 7); // false
+
+console.log(5 != 5); // false
+console.log(5 != 10); // true
+
+console.log("5" == "5"); // true
+console.log("5" != "7"); // true
+
+console.log("5" === "5"); // true
+console.log(5 === "5"); // false
+
+console.log("5" !== "5"); // false
+console.log(5 !== "5"); // true
+
+console.log(2 < 5); // true
+console.log(2 > 5); // false
+console.log(2 > 2); // false
+
+console.log(2 <= 5); // true
+console.log(9 >= 5); // true
+console.log(2 >= 2); // true
