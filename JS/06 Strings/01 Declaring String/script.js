20c69f18186b98dbf1b08d47e80eb347107dde4a
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 18:42:47 +0500

    String Introduction and Declaration

---
+++ JS/06 Strings/01 Declaring String/script.js
@@ -0,0 +1,11 @@
+let str = "Shehram";
+let str2 = 'Javascript is Powerful';
+
+console.log(str);
+console.log(str2);
+console.log(str.length);
+console.log(str2["length"]);
+
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
