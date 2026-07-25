ad0880f495cb265a427eefe1f583b6933a906954
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 12:12:34 +0500

    Learned Assignment Operators

---
+++ JS/03 Operators/02 Assignment Operators/script.js
@@ -0,0 +1,17 @@
+let a = 20;
+
+a += 5; // 25
+a -= 4; // 21
+a *= 3; // 63
+a /= 7; // 9
+a %= 5  // 4
+a **= 3 // 64
+
+// Unary Assignments
+console.log(++a); // 65
+console.log(a++); // 65
+console.log(a);   // 66
+
+console.log(--a); // 65
+console.log(a--); // 65
+console.log(a);   // 64
