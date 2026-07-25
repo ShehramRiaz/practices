e65cc7edb481f0c4433075dbe3b6b7f8d5baddda
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 11:53:52 +0500

    Learned Arithmetic Operators

---
+++ JS/03 Operators/01 Arithmetic Operators/script.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 2;
+
+console.log("a = ", a);
+console.log("b = ", b);
+
+console.log("a + b = ", a + b);     // 7
+console.log("a - b = ", a - b);     // 3
+console.log("a * b = ", a * b);     // 10
+console.log("a / b = ", a / b);     // 2.5
+console.log("a % b = ", a % b);     // 1
+console.log("a ** b = ", a ** b);   // 25
