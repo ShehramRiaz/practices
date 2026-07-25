673b7a239ddc23564f1116579b70c3069b0011e4
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 11:26:52 +0500

    Learned How to use let and const and avoid var keyword

---
+++ JS/02 Variables/05 Using let, const and var/script.js
@@ -0,0 +1,13 @@
+var name = "Shehram";
+var name = "Shehram Riaz";
+name = "Shehru";
+console.log(name);
+
+let age = 19;
+// let age = 24; // Error
+age = 20
+console.log(age);
+
+const gender = "Male";
+// gender = "Female"; // Error
+console.log(gender);
