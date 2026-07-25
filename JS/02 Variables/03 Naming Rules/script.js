10c3bd631834e39cd426ed8dfc310dc5a73d9ed6
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 11:25:34 +0500

    Learned Identifier Naming Rules

---
+++ JS/02 Variables/03 Naming Rules/script.js
@@ -0,0 +1,12 @@
+name = "Shehram";
+NAME = "Shehram Riaz";
+
+subject1 = "Computer Science";
+$section = "B";
+full_name = "Shehram Riaz";
+
+console.log(name);
+console.log(NAME);
+console.log(subject1);
+console.log($section);
+console.log(full_name);
