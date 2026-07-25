2893328fdab9fc6ad0d75d4df986cd89067129cd
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-25 11:25:12 +0500

    Learned Different Datatypes used in Javascript

---
+++ JS/02 Variables/02 DataTypes/script.js
@@ -0,0 +1,32 @@
+let age = 19;
+let price = 99.67;
+
+let fullName = "Shehram Riaz";
+
+let isMale = true;
+
+let a = null;
+
+let b = undefined;
+
+let bigNumber = BigInt(9938438);
+
+let unique = Symbol("Hello");
+
+const student = {
+    name: "Shehram",
+    age: 19,
+    isMale: true
+}
+
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof fullName);
+console.log(typeof isMale);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof bigNumber);
+console.log(typeof unique);
+
+console.log(typeof student);
+console.log(typeof student.age);
