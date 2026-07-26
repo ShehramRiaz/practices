fe6d0b525d5001543b8985143bbd9a4a9181c0dd
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-26 10:15:18 +0500

    Introduction to Arrays

---
+++ JS/07 Arrays/01 Declaring Array/script.js
@@ -0,0 +1,17 @@
+const colors = ["red", "blue", "green", "yellow", "pink", "purple"];
+const marks = [45, 89, 100, 34, 12, 37, 76, 89, 97, 99];
+const mixed = ["Shehram", 19, true, null, undefined, "Computer Science", false];
+
+console.log(colors);
+console.log(colors.length);
+
+console.log(marks);
+console.log(marks.length);
+
+console.log(mixed);
+console.log(mixed.length);
+
+colors[1] = "black";
+console.log(colors);
+
+console.log(mixed[2]);
