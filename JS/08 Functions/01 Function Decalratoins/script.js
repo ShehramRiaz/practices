93fbca271e885ef092b0bdb698eedd6e2532656b
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-26 14:24:45 +0500

    Learned Functions Declarations

---
+++ JS/08 Functions/01 Function Decalratoins/script.js
@@ -0,0 +1,25 @@
+function greet() {
+    console.log("Hello!");
+    console.log("I am Learning JavaScript");
+    console.log("I love JavaScript");
+}
+
+greet();
+greet();
+greet();
+greet();
+
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+
+sayHello("Shehram");
+sayHello("JavaScript");
+sayHello("World");
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(4, 7));
+console.log(add(49, 71));
