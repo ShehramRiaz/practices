da5cc3e882d0826b79ae19e5240fd1f1c395134e
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-27 13:07:16 +0500

    Toggle Theme Mode

---
+++ JS/Toggle Theme Modes/script.js
@@ -0,0 +1,9 @@
+const toggleModeBtn = document.getElementById("toggleModeBtn");
+
+toggleModeBtn.addEventListener("click", () => {
+    if (document.body.classList.contains("dark")) {
+        document.body.classList.remove("dark");
+    } else {
+        document.body.classList.add("dark");
+    }
+});
