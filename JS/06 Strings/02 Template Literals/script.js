6eba30ba78ac2391fed1c229ea409ea200ff17d2
Shehram Riaz <shehramriaz870@gmail.com>
2026-07-31 20:21:47 +0500

    Learned Nested Template Literals and Tagged Template Function

--- JS/06 Strings/02 Template Literals/script.js
+++ JS/06 Strings/02 Template Literals/script.js
@@ -5,3 +5,14 @@ const item = {
 
 const templateLiteral = `The cost of ${item.name} is RS ${item.price}`;
 console.log(templateLiteral);
+
+// Nested Template Literals
+const age = 19;
+console.log(`Your age is ${age > 18 ? `Greater` : `Smaller`} than 18`);
+
+function tagTemplate(strings, ...values) {
+    console.log(strings);
+    console.log(values);
+}
+
+tagTemplate`My name is ${`Shehram`} and\nI am Studying ${`Template Literals`}`;
