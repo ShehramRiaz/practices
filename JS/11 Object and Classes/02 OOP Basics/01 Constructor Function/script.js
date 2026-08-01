17ca77f8ab4812b722218165a3d78a51a579fc52
Shehram Riaz <shehramriaz870@gmail.com>
2026-08-01 18:47:02 +0500

    Learned Constructor Function

---
+++ JS/11 Object and Classes/02 OOP Basics/01 Constructor Function/script.js
@@ -0,0 +1,9 @@
+function Student(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const s1 = new Student("Shehram", 19);
+const s2 = new Student("Shahsawar", 20);
+console.log(s1);
+console.log(s2);
