9b5441143127af1b71062146fd9937f5bd85aca6
Shehram Riaz <shehramriaz870@gmail.com>
2026-08-04 12:31:29 +0500

    feat: add type annotation examples

---
+++ TS/src/02_Type_Annotations.ts
@@ -0,0 +1,48 @@
+/**
+ * ===========================================
+ * Lesson 2 - Type Annotations
+ * ===========================================
+ */
+
+// String
+let firstName: string = "Shehram";
+let language: string = "TypeScript";
+
+// Number
+let age: number = 20;
+let price: number = 199.99;
+
+// Boolean
+let isStudent: boolean = true;
+let hasCompletedHTML: boolean = true;
+
+// Reassigning correct values
+firstName = "Ali";
+age = 21;
+isStudent = false;
+
+// Uncomment these one by one to see TypeScript errors
+
+// firstName = 100;
+// age = "Twenty";
+// isStudent = "Yes";
+
+console.log(firstName);
+console.log(language);
+console.log(age);
+console.log(price);
+console.log(isStudent);
+console.log(hasCompletedHTML);
+
+// Other Examples
+let myCountry: string = "Pakistan";
+let favoriteEditor: string = "VSCode";
+let favoriteProgrammingLanguage: string = "TypeScript";
+let myAge: number = 19;
+let isEnjoyingWebDevelopment: boolean = true;
+
+console.log(myCountry);
+console.log(favoriteEditor);
+console.log(favoriteProgrammingLanguage);
+console.log(myAge);
+console.log(isEnjoyingWebDevelopment);
