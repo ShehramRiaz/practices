71dfce58e311af6ce9e8dba63b37efe0e792b626
Shehram Riaz <shehramriaz870@gmail.com>
2026-08-04 11:57:05 +0500

    feat: add TypeScript introduction examples

---
+++ TS/src/01_Introduction.ts
@@ -0,0 +1,34 @@
+/**
+ * ===========================================
+ * Lesson 1 - Introduction to TypeScript
+ * ===========================================
+ *
+ * TypeScript is a superset of JavaScript.
+ * Every valid JavaScript program is also valid TypeScript.
+ *
+ * TypeScript adds:
+ * - Static type checking
+ * - Better autocompletion
+ * - Better error detection
+ * - Safer refactoring
+ */
+
+// JavaScript code (also valid TypeScript)
+console.log("Hello from TypeScript!");
+
+// Variables
+let username = "Shehram";
+let age = 20;
+let isStudent = true;
+
+console.log(username);
+console.log(age);
+console.log(isStudent);
+
+let favoriteLanguage = "TypeScript";
+let currentYear = 2026;
+let enjoyCoding = true;
+
+console.log(favoriteLanguage);
+console.log(currentYear);
+console.log(enjoyCoding);
