f38ce583a7826c140d9fffe9d09172de600acf34
Shehram Riaz <shehramriaz870@gmail.com>
2026-09-23 20:50:51 +0500

    solve: sum of two numbers

---
+++ DSA C++/01_Variables_DataTypes_Operators/Practice_Questions/01_Sum_of_Two_Numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+
+  cout << "Enter a: ";
+  cin >> a;
+
+  cout << "Enter b: ";
+  cin >> b;
+
+  int sum = a + b;
+
+  cout << a << " + " << b << " = " << sum << endl;
+
+  return 0;
+}
