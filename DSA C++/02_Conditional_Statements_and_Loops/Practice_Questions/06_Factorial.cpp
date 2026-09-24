ba1db527dd3fcf8634130b16762eb5836ecea2a7
Shehram Riaz <shehramriaz870@gmail.com>
2026-09-24 22:28:02 +0500

    learn: find factorial

---
+++ DSA C++/02_Conditional_Statements_and_Loops/Practice_Questions/06_Factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 5, fact = 1;
+
+  for (int i = 2; i <= n; i++)
+  {
+    fact *= i;
+  }
+
+  cout << n << "! = " << fact << endl;
+
+  return 0;
+}
