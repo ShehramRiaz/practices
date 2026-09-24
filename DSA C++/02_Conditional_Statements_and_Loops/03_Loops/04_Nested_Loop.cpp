732fa1fd2e5f57035f213fbfaa50d58d5964a645
Shehram Riaz <shehramriaz870@gmail.com>
2026-09-24 22:20:44 +0500

    learn: nested loop

---
+++ DSA C++/02_Conditional_Statements_and_Loops/03_Loops/04_Nested_Loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 4;
+  for (int i = 0; i < n; i++)
+  {
+    int m = 3;
+    for (int j = 0; j < m; j++)
+    {
+      cout << "*";
+    }
+
+    cout << endl;
+  }
+}
